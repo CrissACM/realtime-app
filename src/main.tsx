feb47553c48a22f3c8fa42f907d4009a04df5a10
crissacm <alex.ccm7Z@gmail.com>
2025-06-23 22:26:19 -0400

    xd

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -9,6 +10,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
+      <Notifications />
       <App />
     </MantineProvider>
   </StrictMode>,
